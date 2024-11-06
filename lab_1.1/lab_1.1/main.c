ec5022aa44d57e4c927812b6bf0a563136d02e0c
Ognyan Ignatov <ognyan.ignatov@gmail.com>
2024-11-06 10:42:43 +0100

    Initial commit

---
+++ lab_1.1/lab_1.1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab_1.1
+//
+//  Created by Owner on 11/6/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("LAB 1.1 \n\n");
+    return 0;
+}
